aea3fdcc892565d40ace16b6f08a80da175226b0
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-08-31 11:12:36 +0100

    feat: initial commit for src/components/PaletteGenerator.js

---
+++ src/components/PaletteGenerator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PaletteGenerator = () => {
+    // Logic for generating palettes will be added here
+    return (
+        <div>
+            <h2>Palette Generator</h2>
+            {/* UI Elements to interact with palette generation */}
+        </div>
+    )
+};
+
+export default PaletteGenerator;
