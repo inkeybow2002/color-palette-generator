6dd93c14b13b5280e6e1da534a566d565794bfbb
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-08-31 11:12:33 +0100

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Color Palette Generator</h1>
+      {/* Add components here */}
+    </div>
+  );
+}
+
+export default App;
